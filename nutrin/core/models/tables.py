937759a06d2419dc82b30b1e44ffe50df9507dac
Emerson Carbonaro <emersonscarbonaro@gmail.com>
2018-05-03 10:49:35 -0300

    tabela Paciente

--- nutrin/core/models/tables.py
+++ nutrin/core/models/tables.py
@@ -8,6 +8,9 @@ class User(db.Model):
     password = db.Column(db.String)
     name = db.Column(db.String)
     email = db.Column(db.String, unique=True)
+    tipo = db.Column(db.String(1))
+
+    #tipo: N - nutricionista, P - paciente, A - admin  
 
     @property
     def is_authenticated(self):
@@ -24,11 +27,12 @@ class User(db.Model):
     def get_id(self):
         return str(self.id)
 
-    def __init__(self, username, password, name, email):
+    def __init__(self, username, password, name, email, tipo):
         self.username = username
         self.password = password
         self.name = name
         self.email = email
+        self.tipo = tipo
     
     def __repr__(self):
         return "<User {0}>".format(self.username)
@@ -36,9 +40,21 @@ class User(db.Model):
 class Paciente(User):
     __tablename__ = "pacientes"
 
-    dataNascimento = db.Column(db.String)
+    dataNascimento = db.Column(db.DateTime)
     sexo = db.Column(db.String)
     cidade = db.Column(db.String)
     profissao = db.Column(db.String)
     celular = db.Column(db.String)
     objetivo = db.Column(db.String)
+
+    def __init__(self, username, password, name, email, dataNascimento, sexo, cidade, profissao, celular, objetivo):
+        super().__init__(username, password, name, email, "P")
+        self.dataNascimento = dataNascimento
+        self.sexo = sexo
+        self.cidade = cidade
+        self.profissao = profissao
+        self.celular = celular
+        self.objetivo = objetivo
+    
+    def __repr__(self):
+        return "<Paciente {0}>".format(self.username)

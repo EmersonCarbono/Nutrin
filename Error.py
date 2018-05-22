cb58904e7ea0a5a384fcb2625a6861c8f47a80c5
jose renato santana santos <30847676+renatoallart@users.noreply.github.com>
2018-05-22 16:38:27 -0300

    Erros e o run.py atualizado

---
+++ Error.py
@@ -0,0 +1,35 @@
+from flask import  jsonify
+from core import app,response
+
+
+@app.errorhandler(500)
+def Error500(error):
+    response["Mensagem"] = " Tivemos um Problema logo sera Resolvido "
+    response["Status"] = "Error: {}".format(error)
+    response["Dados"] = " "
+    return jsonify(response)
+    
+@app.errorhandler(404)
+def Error404(error):
+    response["Mensagem"] = " Recurso Invalida "
+    response["Status"] = "Error: {}".format(error)
+    response["Dados"] = " "
+    return jsonify(response)
+
+@app.errorhandler(400)
+def Error400(error):
+    response["Mensagem"] = " Requisição Invalida "
+    response["Status"] = "Error: {}".format(error)
+    response["Dados"] = " "
+    return jsonify(response)
+
+@app.errorhandler(403)
+def Error403(error):
+    response["Mensagem"] = " Acesso Negado "
+    response["Status"] = "Error: {}".format(error)
+    response["Dados"] = " "
+    return jsonify(response)
+    
+
+
+    
---
+++ run.py
@@ -0,0 +1,5 @@
+from core import app, manager
+from Error import *
+
+if __name__ == "__main__":
+    manager.run()
